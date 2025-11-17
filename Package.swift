659d4ccf8b222e0d1bdbbdaaf4921e0aaccc9b81
mmcnamarahqy-mirror <mmcnamara.hqy@gmail.com>
2025-11-17 08:05:30 -0700

    Update Package.swift
    Pinning dependency to TSCoreSDK.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["IdentityOrchestration", "IdentityOrchestration-Dependencies"])
     ],
     dependencies: [
-        .package(url: "https://github.com/TransmitSecurity/core-ios-sdk.git", from: "1.0.31")
+        .package(url: "https://github.com/TransmitSecurity/core-ios-sdk.git", exact: "1.0.31")
     ],
     targets: [
         .binaryTarget(
